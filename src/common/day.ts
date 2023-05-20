a7947cb02a63b3c7102edf0688fad57e4c0b94a8
stevending1st <stevending1st@163.com>
2023-05-20 16:52:50 +0800

    fix: change dayjs to date-and-time

--- src/common/day.ts
+++ src/common/day.ts
@@ -1,16 +1,33 @@
-import dayjs from 'dayjs';
-import customParseFormat from 'dayjs/plugin/customParseFormat';
+import dateAndTime from 'date-and-time';
 
-dayjs.extend(customParseFormat);
-
-export const checkDateIsValid = (date: string, format: dayjs.OptionType) =>
-  dayjs(date, format, true).isValid();
+export const checkDateIsValid = (date: string, format: string) =>
+  dateAndTime.isValid(date, format);
 
 export const checkDateIsyyyyMM = (date: string) =>
   checkDateIsValid(date, 'YYYYMM');
 
-export const checkDayIsBefore = (startDay: string, endDay: string) =>
-  dayjs(startDay).isBefore(endDay);
+export const checkDayIsBefore = (startDay: string, endDay: string) => {
+  console.log(
+    endDay,
+    dateAndTime.parse(endDay, 'YYYYMMDD'),
+    startDay,
+    dateAndTime.parse(startDay, 'YYYYMMDD'),
+    dateAndTime
+      .subtract(
+        dateAndTime.parse(endDay, 'YYYYMMDD'),
+        dateAndTime.parse(startDay, 'YYYYMMDD')
+      )
+      .toDays()
+  );
+  return (
+    dateAndTime
+      .subtract(
+        dateAndTime.parse(endDay, 'YYYYMMDD'),
+        dateAndTime.parse(startDay, 'YYYYMMDD')
+      )
+      .toDays() > 0
+  );
+};
 
 export const checkMonthIsBefore = (startMonth: string, endMonth: string) =>
   checkDayIsBefore(`${startMonth}01`, `${endMonth}01`);
