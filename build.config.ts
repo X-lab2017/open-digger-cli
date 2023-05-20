de6133a7f00b2620e352ffd53471c8881a8ac85d
stevending1st <stevending1st@163.com>
2023-05-20 16:54:03 +0800

    feat: add dependencies to unbuildconfig

--- build.config.ts
+++ build.config.ts
@@ -1,11 +1,14 @@
 import { defineBuildConfig } from 'unbuild';
 
+import packageJson from './package.json';
+
 export default defineBuildConfig({
-  entries: [ 'src/index' ],
+  entries: ['src/index'],
   clean: true,
   declaration: true,
+  dependencies: Object.keys(packageJson.dependencies),
   rollup: {
     emitCJS: true,
-    inlineDependencies: true,
-  },
-})
+    inlineDependencies: true
+  }
+});
