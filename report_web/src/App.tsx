2cc36479c8bd566562c4ae4eb300ab7f2759200f
stevending1st <stevending1st@163.com>
2023-10-07 03:04:26 +0800

    fix: Support part of the data

--- report_web/src/App.tsx
+++ report_web/src/App.tsx
@@ -22,231 +22,282 @@ function App() {
 
       <button onClick={() => window.print()}>print</button>
 
-      <ReactEChartsBasicBar
-        title="OpenRank"
-        xAxisData={Object.keys(msg.metricData.openrank)}
-        seriesData={Object.values(msg.metricData.openrank)}
-      />
+      {msg.metricData.openrank && (
+        <ReactEChartsBasicBar
+          title="OpenRank"
+          xAxisData={Object.keys(msg.metricData.openrank)}
+          seriesData={Object.values(msg.metricData.openrank)}
+        />
+      )}
 
-      <ReactEChartsBasicBar
-        title="Activity"
-        xAxisData={Object.keys(msg.metricData.activity)}
-        seriesData={Object.values(msg.metricData.activity)}
-      />
+      {msg.metricData.activity && (
+        <ReactEChartsBasicBar
+          title="Activity"
+          xAxisData={Object.keys(msg.metricData.activity)}
+          seriesData={Object.values(msg.metricData.activity)}
+        />
+      )}
 
-      <ReactEChartsBasicBar
-        title="Attention"
-        xAxisData={Object.keys(msg.metricData.attention)}
-        seriesData={Object.values(msg.metricData.attention)}
-      />
+      {msg.metricData.attention && (
+        <ReactEChartsBasicBar
+          title="Attention"
+          xAxisData={Object.keys(msg.metricData.attention)}
+          seriesData={Object.values(msg.metricData.attention)}
+        />
+      )}
 
-      <ReactEChartsHeatmapOnCartesian
-        title="Active dates and times"
-        inputData={msg.metricData.active_dates_and_times}
-      />
+      {msg.metricData.active_dates_and_times && (
+        <ReactEChartsHeatmapOnCartesian
+          title="Active dates and times"
+          inputData={msg.metricData.active_dates_and_times}
+        />
+      )}
 
-      <ReactEChartsStarts title="Stars" data={msg.metricData.stars} />
+      {msg.metricData.stars && (
+        <ReactEChartsStarts title="Stars" data={msg.metricData.stars} />
+      )}
 
-      <ReactEChartsStarts
-        title="Technical fork"
-        data={msg.metricData.technical_fork}
-      />
+      {msg.metricData.technical_fork && (
+        <ReactEChartsStarts
+          title="Technical fork"
+          data={msg.metricData.technical_fork}
+        />
+      )}
 
-      <ReactEChartsBasicLine
-        title="Participants"
-        xAxisData={Object.keys(msg.metricData.participants).filter(
-          key => !key.endsWith('raw')
-        )}
-        seriesData={Object.keys(msg.metricData.participants)
-          .filter(key => !key.endsWith('raw'))
-          .map(value => msg.metricData.participants?.[value] ?? 0)}
-      />
+      {msg.metricData.participants && (
+        <ReactEChartsBasicLine
+          title="Participants"
+          xAxisData={Object.keys(msg.metricData.participants).filter(
+            key => !key.endsWith('raw')
+          )}
+          seriesData={Object.keys(msg.metricData.participants)
+            .filter(key => !key.endsWith('raw'))
+            .map(value => msg.metricData.participants?.[value] ?? 0)}
+        />
+      )}
 
-      <ReactEChartsScaleAreaAddAndRemove
-        title="New and inactive contributors"
-        xAxisData={Object.keys(msg.metricData.participants).filter(
-          key => !key.endsWith('raw')
-        )}
-        addData={Object.keys(msg.metricData.participants)
-          .filter(key => !key.endsWith('raw'))
-          .map(value => msg.metricData.new_contributors?.[value] ?? 0)}
-        removeData={Object.keys(msg.metricData.participants)
-          .filter(key => !key.endsWith('raw'))
-          .map(value => msg.metricData.inactive_contributors?.[value] ?? 0)}
-      />
+      {msg.metricData.participants && msg.metricData.inactive_contributors && (
+        <ReactEChartsScaleAreaAddAndRemove
+          title="New and inactive contributors"
+          xAxisData={Object.keys(msg.metricData.participants).filter(
+            key => !key.endsWith('raw')
+          )}
+          addData={Object.keys(msg.metricData.participants)
+            .filter(key => !key.endsWith('raw'))
+            .map(value => msg.metricData.new_contributors?.[value] ?? 0)}
+          removeData={Object.keys(msg.metricData.participants)
+            .filter(key => !key.endsWith('raw'))
+            .map(value => msg.metricData.inactive_contributors?.[value] ?? 0)}
+        />
+      )}
 
-      <NewContributorsDetail
-        title="New Contributors Detail"
-        data={msg.metricData.new_contributors_detail}
-      />
+      {msg.metricData.new_contributors_detail && (
+        <NewContributorsDetail
+          title="New Contributors Detail"
+          data={msg.metricData.new_contributors_detail}
+        />
+      )}
 
-      <ReactEChartsBasicBar
-        title="Bus factor"
-        xAxisData={Object.keys(msg.metricData.bus_factor)}
-        seriesData={Object.values(msg.metricData.bus_factor)}
-      />
+      {msg.metricData.bus_factor && (
+        <ReactEChartsBasicBar
+          title="Bus factor"
+          xAxisData={Object.keys(msg.metricData.bus_factor)}
+          seriesData={Object.values(msg.metricData.bus_factor)}
+        />
+      )}
 
-      <BusFactorDetail
-        title="Bus Factor Detail"
-        data={msg.metricData.bus_factor_detail}
-      />
+      {msg.metricData.bus_factor_detail && (
+        <BusFactorDetail
+          title="Bus Factor Detail"
+          data={msg.metricData.bus_factor_detail}
+        />
+      )}
 
-      <ReactEChartsScaleAreaAddAndRemove
-        title="New and closed Issues"
-        xAxisData={Object.keys(msg.metricData.issues_new)}
-        addData={Object.values(msg.metricData.issues_new)}
-        removeData={Object.values(msg.metricData.issues_closed)}
-      />
+      {msg.metricData.issues_new && msg.metricData.issues_closed && (
+        <ReactEChartsScaleAreaAddAndRemove
+          title="New and closed Issues"
+          xAxisData={Object.keys(msg.metricData.issues_new)}
+          addData={Object.values(msg.metricData.issues_new)}
+          removeData={Object.values(msg.metricData.issues_closed)}
+        />
+      )}
 
-      <ReactEChartsBasicLine
-        title="Issue comments"
-        xAxisData={Object.keys(msg.metricData.issue_comments).filter(
-          key => !key.endsWith('raw')
-        )}
-        seriesData={Object.keys(msg.metricData.issue_comments)
-          .filter(key => !key.endsWith('raw'))
-          .map(value => msg.metricData.issue_comments?.[value] ?? 0)}
-      />
+      {msg.metricData.issue_comments && (
+        <ReactEChartsBasicLine
+          title="Issue comments"
+          xAxisData={Object.keys(msg.metricData.issue_comments).filter(
+            key => !key.endsWith('raw')
+          )}
+          seriesData={Object.keys(msg.metricData.issue_comments)
+            .filter(key => !key.endsWith('raw'))
+            .map(value => msg.metricData.issue_comments?.[value] ?? 0)}
+        />
+      )}
 
-      <ReactEChartsCandlePictureWithAvg
-        title="Issue response time"
-        xAxisData={Object.keys(msg.metricData.issue_response_time.avg)}
-        quantileData={Object.keys(msg.metricData.issue_response_time.avg).map(
-          m =>
+      {msg.metricData.issue_response_time && (
+        <ReactEChartsCandlePictureWithAvg
+          title="Issue response time"
+          xAxisData={Object.keys(msg.metricData.issue_response_time.avg)}
+          quantileData={Object.keys(msg.metricData.issue_response_time.avg).map(
+            m =>
+              [1, 3, 0, 4].map(
+                q => msg.metricData.issue_response_time[`quantile_${q}`][m]
+              )
+          )}
+          avgData={Object.keys(msg.metricData.issue_response_time.avg).map(
+            m => msg.metricData.issue_response_time['avg'][m]
+          )}
+        />
+      )}
+
+      {msg.metricData.issue_resolution_duration && (
+        <ReactEChartsCandlePictureWithAvg
+          title="Issue resolution duration"
+          xAxisData={Object.keys(msg.metricData.issue_resolution_duration.avg)}
+          quantileData={Object.keys(
+            msg.metricData.issue_resolution_duration.avg
+          ).map(m =>
             [1, 3, 0, 4].map(
-              q => msg.metricData.issue_response_time[`quantile_${q}`][m]
+              q => msg.metricData.issue_resolution_duration[`quantile_${q}`][m]
             )
-        )}
-        avgData={Object.keys(msg.metricData.issue_response_time.avg).map(
-          m => msg.metricData.issue_response_time['avg'][m]
-        )}
-      />
+          )}
+          avgData={Object.keys(
+            msg.metricData.issue_resolution_duration.avg
+          ).map(m => msg.metricData.issue_resolution_duration['avg'][m])}
+        />
+      )}
 
-      <ReactEChartsCandlePictureWithAvg
-        title="Issue resolution duration"
-        xAxisData={Object.keys(msg.metricData.issue_resolution_duration.avg)}
-        quantileData={Object.keys(
-          msg.metricData.issue_resolution_duration.avg
-        ).map(m =>
-          [1, 3, 0, 4].map(
-            q => msg.metricData.issue_resolution_duration[`quantile_${q}`][m]
-          )
-        )}
-        avgData={Object.keys(msg.metricData.issue_resolution_duration.avg).map(
-          m => msg.metricData.issue_resolution_duration['avg'][m]
-        )}
-      />
+      {msg.metricData.issue_age && (
+        <ReactEChartsCandlePictureWithAvg
+          title="Issue age"
+          xAxisData={Object.keys(msg.metricData.issue_age.avg)}
+          quantileData={Object.keys(msg.metricData.issue_age.avg).map(m =>
+            [1, 3, 0, 4].map(q => msg.metricData.issue_age[`quantile_${q}`][m])
+          )}
+          avgData={Object.keys(msg.metricData.issue_age.avg).map(
+            m => msg.metricData.issue_age['avg'][m]
+          )}
+        />
+      )}
 
-      <ReactEChartsCandlePictureWithAvg
-        title="Issue age"
-        xAxisData={Object.keys(msg.metricData.issue_age.avg)}
-        quantileData={Object.keys(msg.metricData.issue_age.avg).map(m =>
-          [1, 3, 0, 4].map(q => msg.metricData.issue_age[`quantile_${q}`][m])
+      {msg.metricData.code_change_lines_add &&
+        msg.metricData.code_change_lines_remov && (
+          <ReactEChartsScaleAreaAddAndRemove
+            title="Code change lines"
+            xAxisData={Object.keys(msg.metricData.code_change_lines_add)}
+            addData={Object.values(msg.metricData.code_change_lines_add)}
+            removeData={Object.values(msg.metricData.code_change_lines_remove)}
+          />
         )}
-        avgData={Object.keys(msg.metricData.issue_age.avg).map(
-          m => msg.metricData.issue_age['avg'][m]
-        )}
-      />
 
-      <ReactEChartsScaleAreaAddAndRemove
-        title="Code change lines"
-        xAxisData={Object.keys(msg.metricData.code_change_lines_add)}
-        addData={Object.values(msg.metricData.code_change_lines_add)}
-        removeData={Object.values(msg.metricData.code_change_lines_remove)}
-      />
+      {msg.metricData.code_change_lines_sum && (
+        <ReactEChartsBasicLine
+          title="Code Change Lines Sum"
+          xAxisData={Object.keys(msg.metricData.code_change_lines_sum).filter(
+            key => !key.endsWith('raw')
+          )}
+          seriesData={Object.keys(msg.metricData.code_change_lines_sum)
+            .filter(key => !key.endsWith('raw'))
+            .map(value => msg.metricData.code_change_lines_sum?.[value] ?? 0)}
+        />
+      )}
 
-      <ReactEChartsBasicLine
-        title="Code Change Lines Sum"
-        xAxisData={Object.keys(msg.metricData.code_change_lines_sum).filter(
-          key => !key.endsWith('raw')
-        )}
-        seriesData={Object.keys(msg.metricData.code_change_lines_sum)
-          .filter(key => !key.endsWith('raw'))
-          .map(value => msg.metricData.code_change_lines_sum?.[value] ?? 0)}
-      />
+      {msg.metricData.participants && msg.metricData.change_requests && (
+        <ReactEChartsScaleAreaAddAndRemove
+          title="Change requests(Open PR and Merged PR)"
+          xAxisData={Object.keys(msg.metricData.participants).filter(
+            key => !key.endsWith('raw')
+          )}
+          addData={Object.keys(msg.metricData.participants)
+            .filter(key => !key.endsWith('raw'))
+            .map(key => msg.metricData.change_requests?.[key] ?? 0)}
+          removeData={Object.keys(msg.metricData.participants)
+            .filter(key => !key.endsWith('raw'))
+            .map(key => msg.metricData.change_requests_accepted?.[key] ?? 0)}
+          addColor="#238636"
+          removeColor="#8957e5"
+        />
+      )}
 
-      <ReactEChartsScaleAreaAddAndRemove
-        title="Change requests(Open PR and Merged PR)"
-        xAxisData={Object.keys(msg.metricData.participants).filter(
-          key => !key.endsWith('raw')
+      {msg.metricData.participants &&
+        msg.metricData.change_requests_reviews && (
+          <ReactEChartsBasicBar
+            title="Change requests reviews"
+            xAxisData={Object.keys(msg.metricData.participants).filter(
+              key => !key.endsWith('raw')
+            )}
+            seriesData={Object.keys(msg.metricData.participants)
+              .filter(key => !key.endsWith('raw'))
+              .map(key => msg.metricData.change_requests_reviews?.[key] ?? 0)}
+          />
         )}
-        addData={Object.keys(msg.metricData.participants)
-          .filter(key => !key.endsWith('raw'))
-          .map(key => msg.metricData.change_requests?.[key] ?? 0)}
-        removeData={Object.keys(msg.metricData.participants)
-          .filter(key => !key.endsWith('raw'))
-          .map(key => msg.metricData.change_requests_accepted?.[key] ?? 0)}
-        addColor="#238636"
-        removeColor="#8957e5"
-      />
 
-      <ReactEChartsBasicBar
-        title="Change requests reviews"
-        xAxisData={Object.keys(msg.metricData.participants).filter(
-          key => !key.endsWith('raw')
-        )}
-        seriesData={Object.keys(msg.metricData.participants)
-          .filter(key => !key.endsWith('raw'))
-          .map(key => msg.metricData.change_requests_reviews?.[key] ?? 0)}
-      />
-
-      <ReactEChartsCandlePictureWithAvg
-        title="Change request response time"
-        xAxisData={Object.keys(msg.metricData.change_request_response_time.avg)}
-        quantileData={Object.keys(
-          msg.metricData.change_request_response_time.avg
-        ).map(m =>
-          [1, 3, 0, 4].map(
-            q => msg.metricData.change_request_response_time[`quantile_${q}`][m]
-          )
-        )}
-        avgData={Object.keys(
-          msg.metricData.change_request_response_time.avg
-        ).map(m => msg.metricData.change_request_response_time['avg'][m])}
-      />
-
-      <ReactEChartsCandlePictureWithAvg
-        title="Change request resolution duration"
-        xAxisData={Object.keys(
-          msg.metricData.change_request_resolution_duration.avg
-        )}
-        quantileData={Object.keys(
-          msg.metricData.change_request_resolution_duration.avg
-        ).map(m =>
-          [1, 3, 0, 4].map(
-            q =>
-              msg.metricData.change_request_resolution_duration[
-                `quantile_${q}`
-              ][m]
-          )
-        )}
-        avgData={Object.keys(
-          msg.metricData.change_request_resolution_duration.avg
-        ).map(m => msg.metricData.change_request_resolution_duration['avg'][m])}
-      />
-
-      <ReactEChartsCandlePictureWithAvg
-        title="Change request age"
-        xAxisData={Object.keys(msg.metricData.change_request_age.avg)}
-        quantileData={Object.keys(msg.metricData.change_request_age.avg).map(
-          m =>
+      {msg.metricData.change_request_response_time && (
+        <ReactEChartsCandlePictureWithAvg
+          title="Change request response time"
+          xAxisData={Object.keys(
+            msg.metricData.change_request_response_time.avg
+          )}
+          quantileData={Object.keys(
+            msg.metricData.change_request_response_time.avg
+          ).map(m =>
             [1, 3, 0, 4].map(
-              q => msg.metricData.change_request_age[`quantile_${q}`][m]
+              q =>
+                msg.metricData.change_request_response_time[`quantile_${q}`][m]
             )
-        )}
-        avgData={Object.keys(msg.metricData.change_request_age.avg).map(
-          m => msg.metricData.change_request_age['avg'][m]
-        )}
-      />
+          )}
+          avgData={Object.keys(
+            msg.metricData.change_request_response_time.avg
+          ).map(m => msg.metricData.change_request_response_time['avg'][m])}
+        />
+      )}
 
-      {/**
-       * TODO:
-       * Activity Details
-       */}
-      <ActivityDetail
-        title="Activity Details"
-        data={msg.metricData.activity_details}
-      />
+      {msg.metricData.change_request_resolution_duration && (
+        <ReactEChartsCandlePictureWithAvg
+          title="Change request resolution duration"
+          xAxisData={Object.keys(
+            msg.metricData.change_request_resolution_duration.avg
+          )}
+          quantileData={Object.keys(
+            msg.metricData.change_request_resolution_duration.avg
+          ).map(m =>
+            [1, 3, 0, 4].map(
+              q =>
+                msg.metricData.change_request_resolution_duration[
+                  `quantile_${q}`
+                ][m]
+            )
+          )}
+          avgData={Object.keys(
+            msg.metricData.change_request_resolution_duration.avg
+          ).map(
+            m => msg.metricData.change_request_resolution_duration['avg'][m]
+          )}
+        />
+      )}
+
+      {msg.metricData.change_request_age && (
+        <ReactEChartsCandlePictureWithAvg
+          title="Change request age"
+          xAxisData={Object.keys(msg.metricData.change_request_age.avg)}
+          quantileData={Object.keys(msg.metricData.change_request_age.avg).map(
+            m =>
+              [1, 3, 0, 4].map(
+                q => msg.metricData.change_request_age[`quantile_${q}`][m]
+              )
+          )}
+          avgData={Object.keys(msg.metricData.change_request_age.avg).map(
+            m => msg.metricData.change_request_age['avg'][m]
+          )}
+        />
+      )}
+
+      {msg.metricData.activity_details && (
+        <ActivityDetail
+          title="Activity Details"
+          data={msg.metricData.activity_details}
+        />
+      )}
     </>
   );
 }
--- src/export.ts
+++ src/export.ts
@@ -2,7 +2,7 @@ import puppeteer from 'puppeteer';
 import { createWebServer } from './fe_build';
 import metricData from '../mock/metricData.json';
 
-export const getPDF = async (url: string) => {
+export const getPDF = async (url: string, fileName: string) => {
   const browser = await puppeteer.launch({ headless: 'new' });
   const page = await browser.newPage();
   await page.setUserAgent(
@@ -10,7 +10,7 @@ export const getPDF = async (url: string) => {
   );
   await page.goto(url, { waitUntil: 'networkidle0' });
   await page.pdf({
-    path: 'hn.pdf',
+    path: `${fileName}.pdf`,
     printBackground: true,
     displayHeaderFooter: false,
     format: 'A4',
@@ -27,18 +27,20 @@ export const getPDF = async (url: string) => {
 };
 
 export const exportFun = async () => {
-  console.log('---export----');
   const server = await createWebServer({
     info: {
       owner: 'owner1',
       name: 'test'
     },
-    metricData
+    metricData: {
+      openrank: metricData.openrank,
+      issues_closed: metricData.issues_closed,
+      issues_new: metricData.issues_new
+    }
   });
   await server.listen();
   server.printUrls();
-  console.log('local--->', server?.resolvedUrls?.local[0]);
   const url = server?.resolvedUrls?.local[0];
-  if (url) await getPDF(url);
+  if (url) await getPDF(url, 'hello');
   server.close();
 };
